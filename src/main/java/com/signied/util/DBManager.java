1226fd956b5f11c8d92129a685b5c35ccf9693d5
​appcoding-ux <iy398884@gmail.com>
2023-10-26 21:37:00 +0900

    커밋

---
+++ src/main/java/com/signied/util/DBManager.java
@@ -0,0 +1,38 @@
+package com.signied.util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DBManager {
+	public static Connection getConnection() throws Exception {
+		Connection con = null;
+		Context initContext = new InitialContext();
+		Context envContext = (Context)initContext.lookup("java:/comp/env");
+		DataSource ds = (DataSource)envContext.lookup("jdbc/myoracle");
+		con = ds.getConnection();
+		return con;
+	}
+	
+	public static void close(Connection con, PreparedStatement ps, ResultSet rs) {
+		try {
+			if(con != null) con.close();
+			if(ps != null) ps.close();
+			if(rs != null) rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void close(Connection con, PreparedStatement ps) {
+		try {
+			if(con != null) con.close();
+			if(ps != null) ps.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
