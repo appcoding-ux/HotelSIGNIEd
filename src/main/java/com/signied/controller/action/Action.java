ab84279a41fed01ef788a84b4c792fde0159b683
hyeonjungback <bbackhyeon@gmail.com>
2023-10-26 17:36:06 +0900

    나도 모르겠다.이제

--- src/main/java/com/signied/controller/action/Action.java
+++ src/main/java/com/signied/controller/action/Action.java
@@ -6,4 +6,5 @@ import javax.servlet.http.HttpServletResponse;
 
 public interface Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, Exception;
+
 }
--- src/main/java/com/signied/controller/action/ActionFactory.java
+++ src/main/java/com/signied/controller/action/ActionFactory.java
@@ -1,10 +1,9 @@
 package com.signied.controller.action;
 
+
 public class ActionFactory {
-	private ActionFactory() {
-	}
-	
-	public static ActionFactory instance = new ActionFactory();
+	private static ActionFactory instance = new ActionFactory();
+	private ActionFactory() {}
 	
 	public static ActionFactory getInstance() {
 		return instance;
@@ -12,30 +11,18 @@ public class ActionFactory {
 	
 	public Action getAction(String command) {
 		Action action = null;
+		System.out.println("ActionFactory :" + command);
 		
-		System.out.println(command);
+		if(command.equals("people_num"))
+			action = new PeopleNumAction();
+		else if(command.equals("date_input"))
+			action = new DateInputAction();
+		else if(command.equals("search_room"))
+			action = new SearchRoomList();
+		else if(command.equals("get_date"))
+			action = new GetSystemDateAction();
 		
-		if(command.equals("hotelQnA")) {
-			action = new HotelQnAAction();
-		}else if(command.equals("QnAwrite")) {
-			action = new HotelQnAWriteAction();
-		}else if(command.equals("QnAList")) {
-			action = new HotelQnAListAction();
-		}else if(command.equals("QnA_check")) {
-			action = new HotelQnACheckFormAction();
-		}else if(command.equals("QnA_check_pass")) {
-			action = new HotelQnACheckAction();
-		}else if(command.equals("QnA_view")) {
-			action = new HotelQnAViewAction();
-		}else if(command.equals("QnA_delete")) {
-			action = new HotelQnADeleteAction();
-		}else if(command.equals("QnA_admin_check_form")) {
-			action = new HotelQnAadminCheckForm();
-		}else if(command.equals("QnA_admin_check_pass")) {
-			action = new HotelQnAadminCheckAction();
-		}else if(command.equals("QnA_reply_form_admin")) {
-			action = new HotelQnAReplyFormAction();
-		}
+
 		return action;
 	}
 }
